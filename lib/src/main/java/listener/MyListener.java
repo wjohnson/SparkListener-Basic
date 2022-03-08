f8492ae26f103c8cf5b5fe3cbd182c407d5600ce
Will Johnson <will@willj.co>
2022-03-07 21:35:40 -0600

    Adding core listener operations

--- lib/src/main/java/listener/MyListener.java
+++ lib/src/main/java/listener/MyListener.java
@@ -1,42 +1,47 @@
 package listener;
 
+import java.util.Set;
+
 import org.apache.spark.scheduler.SparkListenerApplicationStart;
 import org.apache.spark.scheduler.SparkListenerEvent;
 import org.apache.spark.scheduler.SparkListenerJobEnd;
 import org.apache.spark.scheduler.SparkListenerJobStart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.concurrent.atomic.AtomicInteger;
-
 
 public class MyListener extends org.apache.spark.scheduler.SparkListener {
 
   private static final Logger log = LoggerFactory.getLogger("MyLogger");
-  private AtomicInteger jobcount = new AtomicInteger(0);
 
-  @Override
-  public void onOtherEvent(SparkListenerEvent event) {
-    log.info("ONOTHER");
-    
+  public MyListener() {
+    log.info("INITIALIZING");
   }
 
   @Override
-  public void onJobStart(SparkListenerJobStart jobStart) {
-    log.info("ON JOB START");
-  
+  public void onOtherEvent(SparkListenerEvent event) {
+    log.info("MYLISTENER: ON OTHER EVENT END");
+    log.info("ON OTHER EVENT of TYPE:",event.getClass().getName());
+
   }
 
   @Override
   public void onJobEnd(SparkListenerJobEnd jobEnd) {
-    log.info("ON JOB END");
-    jobcount.incrementAndGet();
-    log.info(String.format("Jobs so far: %s", jobcount.toString()));
+    log.info("MYLISTENER: ON JOB END");
 
   }
-  
+
+  @Override
+  public void onJobStart(SparkListenerJobStart jobStart) {
+    log.info("MYLISTENER: ON JOB START");
+    Set<String> keys = jobStart.properties().stringPropertyNames();
+    for (String key : keys){
+      log.info(key, jobStart.properties().getProperty(key));
+    }
+  }
+
   @Override
   public void onApplicationStart(SparkListenerApplicationStart applicationStart) {
-    log.info("ON APP START");
+    log.info("MYLISTENER: ON APP START");
   }
 
 }
